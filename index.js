8ad529f304333558197da339d87516ab924ad2c0
Frolov Andrey <frolov@froweb.ru>
2020-08-22 23:07:04 +0300

    made initial slider functions

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ class FroSlider {
   * @param {string} id Identifier of the processed slider.
   * @param {boolean} avtoplay Automatic image change in the slider.
   * @param {number} interval Time interval between image change (in seconds).
-  * 
   */
   constructor (id, avtoplay, interval) {
     this.options = {
@@ -17,7 +16,9 @@ class FroSlider {
     }
     this.play();
   }
-
+  /**
+  * Checking the validity of the types of slider parameters.
+  */
   checkIncoming() {
     if (this.options.id == '' || typeof this.options.id !== 'string') {
       console.log(
@@ -45,12 +46,73 @@ class FroSlider {
     const sliderItems = sliderId.querySelectorAll(".fro__slide");
     return sliderItems;
   }
-
+  /**
+  * Adds a class to display the image.
+  * @param {number} i Number of the desired image in the set.
+  */
+  addView(i) {
+    const sliderId = document.querySelector(`#${this.options.id}`);
+    const sliderItems = this.getItems(sliderId);
+    sliderItems[i].classList.add('active-slide');
+  }
+  /**
+  * Removes a class to hide the image.
+  * @param {number} i Number of the desired image in the set.
+  */
+  removeView(i) {
+    const sliderId = document.querySelector(`#${this.options.id}`);
+    const sliderItems = this.getItems(sliderId);
+    sliderItems[i].classList.remove('active-slide');
+  }
+  /**
+  * Shows the next image.
+  * @param {object} sliderItems NodeList of images in the slider.
+  */
+  setNext(sliderItems) {
+    for (let i = 0; i < sliderItems.length; i++) {
+      if (sliderItems[i].classList.contains('active-slide')) {
+        const end = sliderItems.length - 1;
+        if (i == end) {
+          this.removeView(i);
+          this.addView(0);
+        } else {
+          this.removeView(i);
+          this.addView(++i);
+        }
+      }
+    }
+  }
+  /**
+  * Shows the previous image.
+  * @param {object} sliderItems NodeList of images in the slider.
+  */
+  setPrev(sliderItems) {
+    for (let i = 0; i < sliderItems.length; i++) {
+      if (sliderItems[i].classList.contains('active-slide')) {
+        if (i == 0) {
+          const endSlide = sliderItems.length - 1;
+          this.removeView(i);
+          this.addView(endSlide);
+          i = endSlide;
+        } else {
+          this.removeView(i);
+          this.addView(--i);
+        }
+      }
+    }
+  }
+  /**
+  * Start displaying images.
+  */
   play() {
     const sliderId = document.querySelector(`#${this.options.id}`);
     const sliderItems = this.getItems(sliderId);
     sliderItems[0].classList.add('active-slide');
     this.checkIncoming();
+
+    if (this.options.avtoplay) {
+      setInterval(() => this.setNext(sliderItems), this.options.interval * 1000);
+    }
   }
 }
 
